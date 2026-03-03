ea125e69115e601993da9d30b2dee77c3d43abeb
Dennis Zhou <dennis@kernel.org>
2026-03-03 11:18:59 -0800

    diffview: add plugin

--- init.lua
+++ init.lua
@@ -968,6 +968,8 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  { 'sindrets/diffview.nvim' },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
